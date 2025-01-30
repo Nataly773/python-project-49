43864215da82b69295d5b7d9949a7285209c9012
Nataly <miss.nechaewa@gmail.com>
2025-01-30 23:14:36 +0300

    add brain_games/scripts/brain_calc.py

---
+++ brain_games/games/calc.py
@@ -0,0 +1,15 @@
+import random 
+from brain_games.manual import run_game
+from  brain_games.const import calc_instruction, MATH_SIGNS 
+
+  
+def get_expression_and_answer():
+    num1, num2 = random.randint(1, 100), random.randint(1 ,100)
+    math_signs = random.choice(MATH_SIGNS) 
+    expression = f' {num1} {math_signs} {num2}'
+    result = eval(expression)
+
+    return expression, str(result)
+
+def run_calc_game():
+    run_game(get_expression_and_answer, calc_instruction)

b9c949e1ff7a01bbca1d9d9ad5449579db50489b
Nataly <miss.nechaewa@gmail.com>
2025-02-02 14:32:28 +0300

    add brain_games/scripts/brain_gcd.py

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,13 @@
+import random 
+import math
+from brain_games.manual import run_game
+from brain_games.const import gcd_instruction
+
+def get_nums_and_gcd():
+    num1, num2 = random.randint(1, 100), random.randint(1 ,100)
+    nums = f"{num1} {num2}"
+    gcd = math.gcd(num1,num2)
+    return nums, str(gcd)
+
+def run_gcd_game():
+    run_game(get_nums_and_gcd, gcd_instruction)

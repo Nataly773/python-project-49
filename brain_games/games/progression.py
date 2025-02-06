66d26633a90b96c9f3ef4f281319f79ab89c2830
Nataly <miss.nechaewa@gmail.com>
2025-02-06 21:56:54 +0300

    add brain_games/games/progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,13 +1,13 @@
 import random
 from brain_games.manual import run_game
-from brain_games.const import PROGRRESSION_INSTRRUCTION, MIN_INSTRRUCTION_LENGTH,\
+from brain_games.const import PROGRRESSION_INSTRRUCTION, MIN_INSTRRUCTION_LENGTH, \
 MAX_INSTRRUCTION_LENGTH
 
 
 def get_progression_and_miss_num():
     start, step = random.randint(1, 100), random.randint(1, 100)
-    prog_length = random.randint(MIN_INSTRRUCTION_LENGTH,\
-                                 MAX_INSTRRUCTION_LENGTH)
+    prog_length = random.randint(MIN_INSTRRUCTION_LENGTH, \
+    MAX_INSTRRUCTION_LENGTH)
     miss_index = random.randint(0, prog_length - 1)
     progr = []
     for i in range(prog_length):  

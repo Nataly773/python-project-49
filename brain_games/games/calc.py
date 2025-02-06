7361e0ea16698edd9b8a3c5f8b04d836e2e374cf
Nataly <miss.nechaewa@gmail.com>
2025-02-06 16:11:49 +0300

    add brain_games/games/calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,7 +6,7 @@ from  brain_games.const import calc_instruction, MATH_SIGNS
 def get_expression_and_answer():
     num1, num2 = random.randint(1, 100), random.randint(1 ,100)
     math_signs = random.choice(MATH_SIGNS) 
-    expression = f' {num1} {math_signs} {num2}'
+    expression = f'{num1} {math_signs} {num2}'
     result = eval(expression)
 
     return expression, str(result)

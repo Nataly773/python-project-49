a0239da767ac99d63c5d9482e4f1b05a7df44a17
Nataly <miss.nechaewa@gmail.com>
2025-02-06 21:42:50 +0300

    add brain_games/games/progression.py

--- brain_games/const.py
+++ brain_games/const.py
@@ -3,7 +3,7 @@ EVEN_INSTRRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 CALC_INSTRRUCTION = 'What is the result of the expression?'
 MATH_SIGNS = ('+', '-', '*')
 GCD_INSTRRUCTION = "Find the greatest common divisor of given numbers."
-MIN_INSTRRUCTION_LENGTH, MAX_INSTRRUCTION_LENGTH= 5, 10
+MIN_INSTRRUCTION_LENGTH, MAX_INSTRRUCTION_LENGTH = 5, 10
 PROGRRESSION_INSTRRUCTION = 'What number is missing in the progression?'
 PRIME_INSTRRUCTION = 'Answer "yes" if given number is prime. Otherwise answer \
 "no".'

bd09bf6e81cee40add66444e6c89e87fd1083b3a
Nataly <miss.nechaewa@gmail.com>
2025-02-06 18:52:40 +0300

    add  brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,7 +6,7 @@ from brain_games.const import prime_instruction
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, (num * 0,5) + 1):
+    for i in range(2, (num // 2) + 1):
         if num % i == 0:
             return False
         

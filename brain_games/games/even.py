162a904d56a7e8e9accbcf8c1a9a85239cc2a9a1
Nataly <miss.nechaewa@gmail.com>
2025-02-11 11:35:13 +0300

    add brain_games/scripts/brain_even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,7 +1,4 @@
 from random import randint
-from brain_games.manual import run_game
-
-EVEN_INSTRRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_even(number):
@@ -13,6 +10,3 @@ def get_question_and_answer():
     answer = "yes" if is_even(question) else "no"
     return question, answer
 
-
-def run_even_game():
-    run_game(get_question_and_answer, EVEN_INSTRRUCTION)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,8 +1,10 @@
-from brain_games.games.even import run_even_game
+from brain_games.manual import run_game
+from brain_games.games. even import get_question_and_answer
+EVEN_INSTRRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def main():
-    run_even_game()
+    run_game(get_question_and_answer, EVEN_INSTRRUCTION)
     
  
 if __name__ == "__main__":
